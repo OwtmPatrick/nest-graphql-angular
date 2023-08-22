ee73a3ae0d64d4bdeedf7dcd8b3db9c0bdcd160a
Radik Zangirov <owtmpatrick@gmail.com>
2023-08-22 18:08:03 +0500

    add auth guard

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -6,40 +6,45 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+// import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-import * as bcrypt from 'bcrypt';
+// import * as bcrypt from 'bcrypt';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post('')
-  async create(@Body() { login, password }: CreateUserDto) {
-    const saltOrRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+  //   @Post('')
+  //   async create(@Body() { login, password }: CreateUserDto) {
+  //     const saltOrRounds = 10;
+  //     const hashedPassword = await bcrypt.hash(password, saltOrRounds);
 
-    return await this.usersService.create({ login, password: hashedPassword });
-  }
+  //     return await this.usersService.create({ login, password: hashedPassword });
+  //   }
 
   @Get()
   findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
