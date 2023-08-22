6caceffc2ad6054ae925d36abcb365aec1eb5afc
Radik Zangirov <owtmpatrick@gmail.com>
2023-08-22 12:35:45 +0500

    add user crud operations

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -14,9 +16,10 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ server/src/users/users.service.ts
@@ -0,0 +1,99 @@
+import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    const isUserAlreadyExist = await this.userRepository.findOne({
+      where: {
+        login: createUserDto.login,
+      },
+    });
+
+    if (isUserAlreadyExist) {
+      throw new HttpException(
+        'User with this login already exist',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    try {
+      await this.userRepository.insert({
+        ...createUserDto,
+        firstName: '',
+        lastName: '',
+      });
+
+      return {
+        statusCode: HttpStatus.CREATED,
+        message: `The user with login ${createUserDto.login} was successfully created`,
+      };
+    } catch (e) {
+      throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async findAll() {
+    try {
+      return await this.userRepository.find();
+    } catch (e) {
+      throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async findOne(id: string) {
+    try {
+      return await this.userRepository.findOne({
+        where: {
+          id,
+        },
+      });
+    } catch (e) {
+      throw new HttpException('Unable to find user', HttpStatus.NOT_FOUND);
+    }
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    try {
+      await this.userRepository.update(id, updateUserDto);
+
+      return await this.userRepository.findOne({
+        where: {
+          id,
+        },
+      });
+    } catch (e) {
+      throw new HttpException('Unable to update user', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async remove(id: string) {
+    try {
+      const user = await this.userRepository.findOne({
+        where: {
+          id,
+        },
+      });
+
+      if (!user) {
+        throw new HttpException('Unable to find user', HttpStatus.NOT_FOUND);
+      }
+
+      await this.userRepository.remove(user);
+
+      return {
+        statusCode: HttpStatus.NO_CONTENT,
+        message: `The user was successfully removed`,
+      };
+    } catch (e) {
+      throw new HttpException('Unable to remove user', HttpStatus.BAD_REQUEST);
+    }
+  }
+}
