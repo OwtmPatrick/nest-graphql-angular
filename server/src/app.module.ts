19163b662935318a3c2eeed8f171c9f8024b7c03
Radik Zangirov <owtmpatrick@gmail.com>
2023-08-23 17:27:39 +0500

    remove redundant code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
@@ -14,7 +12,5 @@ import { typeOrmConfig } from './config/typeOrmConfig';
     UsersModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
