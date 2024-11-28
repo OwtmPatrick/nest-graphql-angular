1a5df7ed96a4c4ca1f53b5cc21bb19541ee7cda8
Radik Zangirov <owtmpatrick@gmail.com>
2024-11-28 16:47:10 +0500

    init client

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { AuthService } from './auth.service';
+import { Apollo, gql } from 'apollo-angular';
+
+const GET_PROPERTIES = gql`
+  query getProperties($filterPropertyInput: FilterPropertyInput!) {
+    properties(filterPropertyInput: $filterPropertyInput) {
+      id
+      name
+      wifi
+    }
+  }
+`;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  bio = 'client';
+
+  constructor(private readonly apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: GET_PROPERTIES,
+        variables: {
+          filterPropertyInput: {
+            search: '',
+          },
+        },
+      })
+      .valueChanges.subscribe((result: any) => {
+        console.log(result);
+      });
+  }
+}
